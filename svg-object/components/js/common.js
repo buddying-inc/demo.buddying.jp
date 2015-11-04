c0b011bbf5969bf72f84583600514f7ed60d3ce4
HarukaMunekata <munekata@buddying.jp>
2015-11-04 16:31:27 +0900

    ランダムでの色指定を2種類作成しました。

--- svg-object/components/js/common.js
+++ svg-object/components/js/common.js
@@ -2,6 +2,7 @@ $(function(){
 
   $(window).load(function(){
     var $hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    var $hue02 = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 
     var svg_test = document.getElementById('svg_test').contentDocument;
     var $svg_test = $(svg_test);
@@ -9,7 +10,7 @@ $(function(){
 
     var $ribbon = $('#ribbon', $svg_test);
     var $hat = $('#hat', $svg_test);
-    $ribbon.find('path').attr('fill', $hue);
+    $ribbon.find('path').attr('fill', $hue02);
 
   });
 
