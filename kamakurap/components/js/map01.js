1f10ae2e59c3d5ed2fef2c1652f4c7e025577ec7
HarukaMunekata <munekata@buddying.jp>
2015-11-26 17:46:12 +0900

    画像のパスを変更しました。

--- kamakurap/components/js/map01.js
+++ kamakurap/components/js/map01.js
@@ -48,7 +48,7 @@ function inicializar() {
 
 function create_maker(latlng, html) {
   //アイコンを作成
-   var icon = new google.maps.MarkerImage('components/img/map_icon.png',
+   var icon = new google.maps.MarkerImage('../components/img/map_icon.png',
     new google.maps.Size(36,47),/*アイコンサイズ設定*/
     new google.maps.Point(0,0)  // origin
     );
