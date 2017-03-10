9739ffe6d3f24e4b79f145947f3be544b2a85e35
shark4649 <matsumoto@buddying.jp>
2017-03-10 13:57:09 +0900

    menu修正

--- aff-test-client/index.php
+++ aff-test-client/index.php
@@ -1,9 +1,16 @@
 <?php
 
 $afId = $_GET["afid"] ? $_GET["afid"] : "";
+$oId = $_GET["oid"] ? $_GET["oid"] : "";
 if (empty($afId)) die("error: no id added.");
 
-$content = file_get_contents("http://www.preaf.jp:40080/r.php?afid=".$afId);
+$url = "http://www.preaf.jp:40080/r.php?afid=".$afId;
+
+if (!empty($oid)) {
+    $url .= "&oid=" . $oid;
+}
+
+$content = file_get_contents($url);
 $r = fopen(dirname(__FILE__) . "/logs/".$afId.".log","w");
 fwrite($r,$content);
 fclose($r);
