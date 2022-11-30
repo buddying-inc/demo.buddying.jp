09c44364768af45b3e2632d955367cb29889ee9b
apple-x-co <apple.x.co@gmail.com>
2022-11-30 16:32:50 +0900

    feat: add note RSS reader

--- note-rss/rss.php
+++ note-rss/rss.php
@@ -1,7 +1,8 @@
 <?php
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://note.com/hix_pharmacist/');
+curl_setopt($ch, CURLOPT_URL, 'https://note.com/hix_pharmacist/rss.xml');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
 curl_exec($ch);
